1bf34fc34118c08879f0079d3fbf9f92d3dbc458
Yogesh K <armsvcmode@gmail.com>
2021-07-17 13:11:03 +0530

    Changes: file name,  add new heading, removed and replaced with new variables as needed

--- WebScraping_SecurityCamera/camera_webscraping.py
+++ WebScraping_SecurityCamera/camera_webscraping.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-# =============================================================================
-# Author  : Yogesh K
-# Date    : 10 July 2021
-# Scraping the web for camera info
+# -*- coding:utf-8 -*-
+
+# @Filename:    camera_webscraping.py
+# @Author:      Yogesh K
+# @Time:        13/07/21 9:12 AM
 # =============================================================================
 # Imports
 # =============================================================================
@@ -13,11 +14,12 @@ import http.client
 import re
 import argparse
 from pylogger import pylog
-from urllib.parse import urlparse, urljoin
+from web_link_crawler import extract_all_links
+
 # =============================================================================
 # Global Defines
 # =============================================================================
-URL = "https://www.flipkart.com/search?q=security+camera+for+home"
+URL = "https://www.flipkart.com/search?q=home%20security%20camera"
 
 
 def getUsrInput():
@@ -28,13 +30,14 @@ def getUsrInput():
     :return: None
     """
     layout = [[sg.T("         "), sg.Checkbox('Do you want Night Vision', default=True, key="-IN1-")],
-              [sg.T("         "), sg.Checkbox('Do you want Pan/Tilt', default=True, key="-IN2-")],
+              [sg.T("         "), sg.Checkbox('Do you want Pan', default=True, key="-IN2-")],
               [sg.T("         "), sg.Checkbox('Do you want Wall Mount', default=True, key="-IN3-")],
               [sg.T("         "), sg.Checkbox('Do you want HDD support', default=True, key="-IN4-")],
               [sg.T("         "), sg.Checkbox('Do you want Outdoor support', default=True, key="-IN5-")],
               [sg.T("         "), sg.Checkbox('Do you want 1080p Resolution', default=True, key="-IN6-")],
               [sg.T("         "), sg.Checkbox('Do you want two channels', default=True, key="-IN7-")],
               [sg.T("         "), sg.Checkbox('Do you want white color ', default=True, key="-IN8-")],
+              [sg.T("         "), sg.Checkbox('Do you want Tilt', default=True, key="-IN9-")],
               [sg.T("")], [sg.T("        "), sg.Button('Done', size=(20, 4))], [sg.T("")]
               ]
 
@@ -43,34 +46,41 @@ def getUsrInput():
 
     usrCamFeatures = {  # Initialise a dictionary with default values for user feature set
         'nightVision': True,  # This dict will be used for web scraping later
-        'pan-tilt': True,
+        'pan': True,
         'mount-wall': True,
         'hDD': True,
         'outdoor': True,
         'resolution-1080': True,
-        'channels-2': True,
+        'no-of-channels-2': True,
         'color-white': True,
+        'tilt': True
     }
     while True:
         event, values = window.read()
         usrCamFeatures.update(  # update dict based on user input
             {
                 'nightVision': values["-IN1-"],
-                'pan-tilt': values["-IN2-"],
+                'pan': values["-IN2-"],
                 'mount-wall': values["-IN3-"],
                 'hDD': values["-IN4-"],
                 'outdoor': values["-IN5-"],
                 'resolution-1080': values["-IN6-"],
-                'channels-2': values["-IN7-"],
-                'color-white': values["-IN8-"]
+                'no-of-channels-2': values["-IN7-"],
+                'color-white': values["-IN8-"],
+                'tilt': values["-IN9-"]
             }
         )
         if event == sg.WIN_CLOSED or event == "Done":
             break
 
     window.close()
+    usr_features_to_scrape = []
     for key, value in usrCamFeatures.items():
         print(key, ':', value)
+        if not value:
+            continue
+        usr_features_to_scrape.append(key)
+    return usr_features_to_scrape
 
 
 def extract_scraping_links(source_url):
@@ -79,7 +89,6 @@ def extract_scraping_links(source_url):
     Flipkart camera
     :return: Links to be scraped
     """
-
     logger.info(f'Extracting links from {source_url}')
     page = requests.get(source_url)  # reponse is 200 for success
 
@@ -96,22 +105,19 @@ def extract_scraping_links(source_url):
     # greedy(.*?) double quotes("") to get the exact web links
 
     smartURL = re.findall(r"\"smartUrl\":\".*?\"", soup.prettify())  # is a list
-    only_http_link = []
+    all_http_links = set()    # To get unique list of links
     for link in smartURL:
-        # print(link)  # At this point we have all the url links of the cameras to be scraped
-        el = re.findall(r"\"(http:.*?)\"",
-                        link)  # As we use group, findall will only return matched group and not quotes "
-        print(el)
-        only_http_link.append(el)
+        #print(link)  # At this point we have all the url links of the cameras to be scraped
+        el = re.search(r"\"(http:.*?)\"",
+                        link)  # As we use group, search()/findall() will only return matched group and not quotes "
 
-    return only_http_link
+        # re.search() returns a string with double quotes("sssss") for the match. This double quotes around the match
+        # needs to be removed , as list append() again adds a single quote to the string.
+        link_remove_quote = re.sub("\"", "", el.group())  # el.group() gives the exact matched result after re.search()
+        all_http_links.add(link_remove_quote)
 
-def scrape_all_links(links):
-    """
-    scrape_all_links() will scrape all the links
-    :param: links All the links to be scraped
-    :return: Links to be scraped
-    """
+    return all_http_links
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -120,12 +126,12 @@ def main():
     parser.add_argument('-u', type=str, help='Base site url',
                         default=URL)
     args = parser.parse_args()
-    getUsrInput()
+    usr_features_to_scrape = getUsrInput()
     logger.debug('This is a main debug message')
     # Start extracting the links to scrape
     links_to_scrape = extract_scraping_links(args.u)
     # Now scrape all the links
-    scrape_all_links(links_to_scrape, args.u)
+    extract_all_links(links_to_scrape, args.u, usr_features_to_scrape)
 
 
 # Now scraping begins for every url
